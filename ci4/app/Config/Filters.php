1ef3958ebd32fe2d4b34c8eb4d1b4152cc376cec
tenzap <fabstz-it@yahoo.fr>
2026-05-31 16:46:58 +0200

    enable csrf

--- ci4/app/Config/Filters.php
+++ ci4/app/Config/Filters.php
@@ -74,7 +74,7 @@ class Filters extends BaseFilters
     public array $globals = [
         'before' => [
             // 'honeypot',
-            // 'csrf',
+            'csrf',
             // 'invalidchars',
         ],
         'after' => [
