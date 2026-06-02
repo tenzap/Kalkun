f2bc24657435fbcae91f66034a3f89417e6ffc63
tenzap <fabstz-it@yahoo.fr>
2026-06-02 19:28:12 +0200

    UserModel: replace db method

--- ci4/app/Models/UserModel.php
+++ ci4/app/Models/UserModel.php
@@ -127,7 +127,7 @@ class UserModel extends Model {
 			$q->set('delivery_report', 'default');
 			$q->set('language', 'english');
 			$q->set('conversation_sort', 'asc');
-			$q->set('id_user', $q->insert_id());
+			$q->set('id_user', $q->insertID());
 
 			$q->insert();
 		}
